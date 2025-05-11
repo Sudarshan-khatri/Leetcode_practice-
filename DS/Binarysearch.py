d6eb6f51bfe17fcc78266cb95fdf4bb11398f131
Sudharshan <khatrisudharshan321@gmail.com>
2025-05-11 14:50:09 +0545

    html practice

--- DS/Binarysearch.py
+++ DS/Binarysearch.py
@@ -5,15 +5,15 @@ class BinarySearch():
         highest=len(num)-1
         while lowest<=highest:
             mid=(lowest+highest)//2
-            if num[mid]>key:
+            if num[mid]<key:
                 highest=mid-1
-            elif num[mid]<key:
+            elif num[mid]>key:
                 lowest=mid+1
             else:
                 return f'Position:{mid+1} and value:{num[mid]}'
         return 'key not found'
 
 #instnce of class :
-num=[1,2,3,4,5,6,7,8,9]
+num=[9,8,7,6,5,4,3,2,1]
 obj1=BinarySearch()
 print(obj1.Binary(num,3))
